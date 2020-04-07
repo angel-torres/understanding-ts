5a2e7b815c6de3e919e57ee1577afd15dafa1f96
angel-torres <alas4224@gmail.com>
2020-04-07 12:43:25 -0500

    finished solution for typedStack.ts

--- typed-stack/typedStack.ts
+++ typed-stack/typedStack.ts
@@ -8,22 +8,37 @@ interface IStack<T> {
 
 interface IStackNode<T> {
     data: T;
-    next: IStack<T>
+    next: IStackNode<T>
 }
 
 class Stack<T> implements IStack<T> {
     private head: IStackNode<T> 
 
-    push(item: T): IStack<T> {
+    // example of function overloading
+    push(item: T): IStack<T> 
+    push(items: T[]): IStack<T>
+    // implementation not accessible directly
+    push(itemOrArray: T | T[]): IStack<T> {
+        if(itemOrArray instanceof Array) {
+            itemOrArray.forEach(item =>  this.push(item))
+        } else {
+            let n = {
+                data: itemOrArray as T,
+                next: this.head
+            }
+            this.head = n
 
-    }
+        }
 
-    push(items: T[]): IStack<T> {
+        return this;
 
     }
 
     pop(): T | undefined {
-
+        let n = this.head;
+        if(!n) return undefined
+        this.head = this.head.next;
+        return n.data;
     }
 
     length(): number {
@@ -41,9 +56,10 @@ class Stack<T> implements IStack<T> {
     print(): void {
         let n = this.head;
         while(n) {
-            console.log(n.data)
             n = n.next;
         }
     }
 
 }
+
+let newStack = new Stack<number>();
